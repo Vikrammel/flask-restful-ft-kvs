0a8c0fe194d2cff52a6ff8448acc5a5132d585de
Tom Dubiner <tdubiner@ucsc.edu>
2017-11-14 19:54:11 +0000

    progress. incomplete.

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from flask import Flask
 from flask import request, abort, jsonify, json
 from flask_restful import Resource, Api
 import re, sys, os, requests
+import datetime
 
 app = Flask(__name__)
 api = Api(app)
 newline = "&#13;&#10;"
+K = os.environ.get('K')
 EXIP = os.environ.get('IP')
 PORT = os.environ.get('PORT')
 mainAddr = os.environ.get('MAINIP')
@@ -22,28 +24,26 @@ class Handle(Resource):
         def get(self, key):
             #If key is not in dict, return error.
             if key not in d:
-                print("pingers")
                 return {'result': 'Error', 'msg': 'Key does not exist'}, 404
             #If key is in dict, return its corresponding value.
-            return {'result': 'Success', 'value': d[key]}, 200
+            return {'result': 'Success', 'value': d[key], 'node_id': IP, 'causal_payload': , 'timestamp': datetime.datetime.now().time()}, 200
 
         #Handles PUT request
         def put(self, key):
+			#Makes sure a value was actually supplied in the PUT.
             try:
                 value = request.form['val']
             except:
-                value = ''
                 pass
-            #Makes sure a value was actually supplied in the PUT.
             if not value:
                 return {'result': 'Error', 'msg': 'No value provided'}, 403
+			
             #Restricts key length to 1<=key<=200 characters.
             if not 1 <= len(str(key)) <= 200:
                 return {'result': 'Error', 'msg': 'Key not valid'}, 403
             #Restricts key to alphanumeric - both uppercase and lowercase, 0-9, and _
             if not re.match(r'^\w+$', key):
                 return {'result': 'Error', 'msg': 'Key not valid'}, 403
-
             #Restricts value to a maximum of 1Mbyte.
             if sys.getsizeof(value) > 1000000:
                 return {'result': 'Error', 'msg': 'Object too large. Size limit is 1MB'}, 403
@@ -72,23 +72,19 @@ class Handle(Resource):
             #try requesting primary
             try:
                 response = requests.get(http_str + mainAddr + '/kv-store/' + key)
-            except requests.exceptions.RequestException as exc: #handle primary failure upon put request
-                return {'result': 'Error','msg': 'Server unavailable'}, 500
+            except requests.exceptions.RequestException as exc: #handle primary failure upon get request
+                return {'result': 'Error', 'msg': 'Server unavailable'}, 500
             return response.json()
 
         def put(self,key):
-            try:
-                value = request.form['val']
-            except:
-                value = ''
-                pass
-            #Makes sure a value was actually supplied in the PUT.
-            if not value:
-                return {'result': 'Error', 'msg': 'No value provided'}, 403
+            #try requesting primary
+			if 'val' not in globals():
+			    return {'result': 'Error', 'msg': 'No value provided'}, 403
+            value = str(request.form['val'])
             try:
                 response = requests.put((http_str + mainAddr + '/kv-store/' + key), data = {'val': value})
             except requests.exceptions.RequestException as exc: #handle primary failure upon put request
-                return {'result': 'Error','msg': 'Server unavailable'}, 500
+                return {'result': 'Error', 'msg': 'Server unavailable'}, 500
             return response.json()
 
         def delete(self, key):
@@ -96,9 +92,9 @@ class Handle(Resource):
             try:
                 response = requests.delete(http_str + mainAddr + '/kv-store/' + key)
             except requests.exceptions.RequestException as exc: #handle primary failure upon delete request
-                return {'result': 'Error','msg': 'Server unavailable'}, 500
+                return {'result': 'Error', 'msg': 'Server unavailable'}, 500
             return response.json()
-api.add_resource(Handle, '/kv-store/<key>')
 
+api.add_resource(Handle, '/kv-store/<key>')
 if __name__ == "__main__":
     app.run(host=EXIP, port=PORT)
